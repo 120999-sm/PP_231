06db14fff20f0178592ed635fafd683d6be97d40
120999-sm <slavik.mixailov1999@gmail.com>
2023-11-18 22:39:12 +0300

    update task

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,19 +1,65 @@
 package web.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
 
 @Controller
+@RequestMapping()
 public class UserController {
-
-
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "/hello";
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
+    @GetMapping("/")
+    public String showUsers(Model model){
+        model.addAttribute("users", userService.getUsersList());
+        return "users";
+    }
+    @GetMapping("/new")
+    public String createNewUser (Model model) {
+        model.addAttribute(new User());
+       return "new_user";
+   }
 
+    @PostMapping("/")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String remove(@ModelAttribute("id") int id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showAndEdit(ModelMap model, @PathVariable int id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "edit_user";
+    }
+
+    @PostMapping("/edit")
+    public String editUser(@ModelAttribute("user") User user) {
+        userService.editUser(user);
+        return "redirect:/";
+    }
+    @PostMapping("/delete")
+    public String deleteUser(@ModelAttribute("id") int id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
 }
+
+
+
+
 
 
